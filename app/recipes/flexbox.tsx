34b0d54b6bd7da26fb263b538359fceacdbd678a
PassSam25 <sam.passalacqua@comcast.net>
2025-04-25 11:38:49 -0700

    Add files via upload

---
+++ app/recipes/flexbox.tsx
@@ -0,0 +1,5 @@
+<div class="flex-container">
+  <div>Chocholate Chip Cookies</div>
+  <div>Omelet</div>
+  <div>Quesadilla</div>
+</div>
---
+++ app/recipes/page.tsx
@@ -0,0 +1,38 @@
+import { title } from "@/components/primitives";
+
+export default function RecipesPage() {
+  return (
+    <div>
+      <html>
+        <body>
+            <h2>Chocholate Chip Cookies</h2>
+            <img src="https://www.ihearteating.com/wp-content/uploads/2016/08/Easiest-Chocolate-Chip-Cookies-800-2.jpg" alt="Image Unable to Load" width="250" height="250"></img>
+            <p>Step 1: Melt and whisk butter.</p>
+            <p>Step 2: Add and whisk sugar.</p>
+            <p>Step 3: Mix in vanilla extract and egg.</p>
+            <p>Step 4: Add the flour, baking soda, and salt. Stir but do not overmix.</p>
+            <p>Step 5: Stir in the chocolate chips, and then scoop the dough. Place on baking sheet.</p>
+            <p>Step 6: Bake for 7-10 minutes.</p>
+            <br />
+            <h2>Omelet</h2>
+            <img src="https://www.allrecipes.com/thmb/thpVpY8VqH24xheC74JIg5ySvB0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/4543821-deluxe-bacon-onion-omelet-Christina-4x3-1-d7ef1a4df9444b30b1eb14d63ab2d085.jpg" alt="Image Unable to Load" width="250" height="250"></img>
+            <p>Step 1: Prep eggs.</p>
+            <p>Step 2: Melt butter and oil in the pan.</p>
+            <p>Step 3: Pour and move eggs into pan.</p>
+            <p>Step 4: Add toppings and fold the omelet.</p>
+            <p>Step 5: Flip omelet.</p>
+            <p>Step 6: Let cool and enjoy.</p>
+            <br />
+            <h2>Quesadilla</h2>
+            <img src="https://gimmedelicious.com/wp-content/uploads/2021/02/Buffalo-Chicken-Quesadillas-2-1097x1536.jpg" alt="Image Unable to Load" width="250" height="250"></img>
+            <p>Step 1: Spray pan with cooking oil</p>
+            <p>Step 2: Place down tortilla.</p>
+            <p>Step 3: Add Cheese and chicken.</p>
+            <p>Step 4: Place second tortilla on top.</p>
+            <p>Step 5: Flip quesadilla when one side is brown.</p>
+            <p>Step 6: Let cool and enjoy.</p>
+        </body>
+      </html>
+    </div>
+  );
+}
